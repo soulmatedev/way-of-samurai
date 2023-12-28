8339ba72a588f5e0d75109c7204f1615ec5ee4b9
soulmatedev <khokhlov.dev@gmail.com>
2023-12-28 17:09:14 +0500

    Делит на компоненты (Header, Navbar, Profile)

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+    <header className="header">
+        <img
+            src='https://png.pngtree.com/png-clipart/20230207/original/pngtree-beauty-logo-design-png-image_8947095.png'
+            alt=''/>
+    </header>
+)
+
+export default Header;
