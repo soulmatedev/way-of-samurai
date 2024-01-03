2355ff59b6298c265b394c206a8d3397ac07cab0
soulmatedev <khokhlov.dev@gmail.com>
2024-01-03 21:59:37 +0500

    Верстает Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import React from "react";
 import style from './Dialogs.module.css';
 const Dialogs = () => {
 	return (
-		<div>
-			Dialogs
+		<div className={style.dialogs}>
+			<div className={style.items}>
+				<div className={style.dialog + ' ' + style.active}>
+					Dimych
+				</div>
+				<div className={style.dialog}>
+					Andrey
+				</div>
+				<div className={style.dialog}>
+					Sveta
+				</div>
+				<div className={style.dialog}>
+					Sasha
+				</div>
+				<div className={style.dialog}>
+					Viktor
+				</div>
+				<div className={style.dialog}>
+					Valera
+				</div>
+			</div>
+			<div className={style.messages}>
+				<div className={style.message}>Hi</div>
+				<div className={style.message}>How are you?</div>
+				<div className={style.message}>Hia</div>
+			</div>
 		</div>
 	)
 }
