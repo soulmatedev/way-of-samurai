dc409b4e76bca39badc6fdbbffdcb68cede3856a
soulmatedev <khokhlov.dev@gmail.com>
2024-01-05 21:21:44 +0500

    Реализует вынос данных в объекты

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -13,23 +13,38 @@ const DialogItem = (props) => {
 const Message = (props) => {
 	return <div className={style.message}>{props.message}</div>
 }
+
+let dialogsData = [
+	{id: 1, name: 'Dimych'},
+	{id: 2, name: 'Andrey'},
+	{id: 3, name: 'Sveta'},
+	{id: 4, name: 'Sasha'},
+	{id: 5, name: 'Viktor'},
+	{id: 6, name: 'Valera'},
+]
+
+let messagesData = [
+	{id: 1, message: 'Hi'},
+	{id: 2, message: 'How are you?'},
+	{id: 3, message: 'Hia'},
+]
 const Dialogs = () => {
 	return (
 		<div className={style.dialogs}>
 			<div className={style.items}>
 
-				<DialogItem name="Dimych" id="1" />
-				<DialogItem name="Andrey" id="2" />
-				<DialogItem name="Sveta" id="3" />
-				<DialogItem name="Sasha" id="4" />
-				<DialogItem name="Viktor" id="5" />
-				<DialogItem name="Valera" id="6" />
+				<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+				<DialogItem name={dialogsData[1].name} id={dialogsData[2].id}/>
+				<DialogItem name="Sveta" id="3"/>
+				<DialogItem name="Sasha" id="4"/>
+				<DialogItem name="Viktor" id="5"/>
+				<DialogItem name="Valera" id="6"/>
 
 			</div>
 			<div className={style.messages}>
-				<Message message="Hi" />
-				<Message message="How are you?" />
-				<Message message="Hia" />
+				<Message message={messagesData[0].message}/>
+				<Message message={messagesData[1].message}/>
+				<Message message={messagesData[2].message}/>
 			</div>
 		</div>
 	)
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import style from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+	let postData = [
+		{id: 1, message: 'Hi, how are you?', likesCount: 15},
+		{id: 2, message: "It`s my first post", likesCount: 20},
+	]
+
 	return (
 		<div className={style.posts_block}>
 			<h3>My posts</h3>
@@ -15,11 +21,10 @@ const MyPosts = () => {
 				</div>
 			</div>
 			<div className={style.posts}>
-				<Post message="Hi, how are you?" likes={15}/>
-				<Post message="It's my first post" likes={20}/>
+				<Post message={postData[0].message} likes={postData[0].likesCount}/>
+				<Post message={postData[1].message} likes={postData[1].likesCount}/>
 			</div>
 		</div>
 	)
 }
-
 export default MyPosts;
