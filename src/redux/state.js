e404980774510db94367f1790a07b541778fa022
soulmatedev <khokhlov.dev@gmail.com>
2024-01-17 15:44:10 +0500

    Добавляет reducer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,7 +1,6 @@
-const ADD_POST = 'ADD-POST';
-const ADD_MESSAGE = 'ADD-MESSAGE';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+import profileReducer from "./profileReducer";
+import dialogsReducer from "./dialogsReducer";
+import sidebarReducer from "./sidebarReducer";
 
 let store = {
 	_state: {
@@ -44,39 +43,16 @@ let store = {
 		this._callSubscriber = observer;
 	},
 
-	dispatch (action) {
-		if (action.type === ADD_POST) {
-			let newPost = {
-				id: 5,
-				message: this._state.profilePage.newPostText,
-				likesCount: 0,
-			};
-			this._state.profilePage.posts.push(newPost);
-			this._state.profilePage.newPostText = '';
-			this._callSubscriber(this._state);
-		} else if (action.type === UPDATE_NEW_POST_TEXT) {
-			this._state.profilePage.newPostText = action.newText;
-			this._callSubscriber(this._state);
-		}
-		if (action.type === ADD_MESSAGE) {
-			let newMessage = {
-				id: 3,
-				message: this._state.dialogsPage.newMessageBody,
-			};
-			this._state.dialogsPage.messages.push(newMessage)
-			this._state.dialogsPage.newMessageBody = '';
-			this._callSubscriber(this._state);
-		} else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-			this._state.dialogsPage.newMessageBody = action.body;
-			this._callSubscriber(this._state);
-		}
+	dispatch(action) {
+
+		this._state.profilePage = profileReducer(this._state.profilePage, action)
+		this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action)
+		this._state.sidebar = sidebarReducer(this._state.sidebar, action)
+
+		this._callSubscriber(this._state);
+
 	}
 }
-export const addPostActionCreator = () => ({type: ADD_POST})
-export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text})
-
-export const sendMessageCreator = () => ({type: ADD_MESSAGE})
-export const updateNewMessageBodyCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 
 export default store;
 window.store = store;
