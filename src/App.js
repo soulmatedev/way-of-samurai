00ee6e57f7fe26bbbaeeb388cc47b26a999a1f4f
soulmatedev <khokhlov.dev@gmail.com>
2024-01-19 16:49:45 +0500

    Добавляет контейнерную компоненту для постов

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App(props) {
 							path="/profile"
 							element={
 								<Profile
-									profilePage={props.state.profilePage}
+									store={props.store}
 									dispatch={props.dispatch}
 								/>}
 						/>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import style from './MyPosts.module.css';
 import Post from "./Post/Post";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profileReducer";
 
 const MyPosts = (props) => {
-
 	let postsElements = props.posts.map(post =>
 		<Post message={post.message} likes={post.likesCount}/>)
 
 	let newPostElement = React.createRef();
 	let addPost = () => {
-		props.dispatch(addPostActionCreator());
+		props.addPost();
 	}
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
-		let action = updateNewPostTextActionCreator(text);
-		props.dispatch(action);
+		props.updateNewPostText(text);
 	}
 
 	return (
