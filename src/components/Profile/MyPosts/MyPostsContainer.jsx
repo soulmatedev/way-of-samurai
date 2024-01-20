d121b5150076cef21fb75ee0b55660b216b8cd19
soulmatedev <khokhlov.dev@gmail.com>
2024-01-20 18:55:58 +0500

    Добавляет react-redux в MyPostsContainer

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,24 +1,26 @@
-import React from "react";
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profileReducer";
+import {connect} from "react-redux";
 import MyPosts from "./MyPosts";
 
-const MyPostsContainer = (props) => {
-	let state = props.store.getState();
-	let onAddPost = () => {
-		props.store.dispatch(addPostActionCreator());
+const mapStateToProps = (state) => {
+	return {
+		posts: state.profilePage.posts,
+		newPostText: state.profilePage.newPostText,
 	}
-	let onPostChange = (text) => {
-		let action = updateNewPostTextActionCreator(text);
-		props.store.dispatch(action);
-	}
-
-	return (
-		<MyPosts
-			updateNewPostText={onPostChange}
-			addPost={onAddPost}
-			posts={state.profilePage.posts}
-			newPostText={state.profilePage.newPostText}
-		/>
-	)
 }
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		updateNewPostText: (text) => {
+			let action = updateNewPostTextActionCreator(text);
+			dispatch(action);
+		},
+		addPost: () => {
+			dispatch(addPostActionCreator());
+		}
+	}
+}
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
+
 export default MyPostsContainer;
