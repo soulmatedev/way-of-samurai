4ed0ea6186e4cbaae2867d4d4d955b5d1e95a38c
soulmatedev <khokhlov.dev@gmail.com>
2024-01-30 14:28:00 +0500

    Добавляет метод жизненного цикла в users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,8 +4,7 @@ import userPhoto from "../../assets/images/user.png";
 import axios from "axios";
 
 class Users extends React.Component {
-	constructor(props) {
-		super(props);
+	componentDidMount() {
 		axios.get("https://social-network.samuraijs.com/api/1.0/users")
 			.then(response => {
 				this.props.setUsers(response.data.items);
