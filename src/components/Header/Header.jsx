af9cd2e0bbd31b613e13d637ffeda923d561534b
soulmatedev <khokhlov.dev@gmail.com>
2024-02-12 21:21:38 +0500

    Добавляет авторизацию

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import style from './Header.module.css';
-const Header = () => (
-    <header className={style.header}>
+import styles from './Header.module.css';
+import {NavLink} from "react-router-dom";
+const Header = (props) => (
+    <header className={styles.header}>
         <img
             src='https://png.pngtree.com/png-clipart/20230207/original/pngtree-beauty-logo-design-png-image_8947095.png'
             alt=''/>
+      <div className={styles.login_block}>
+        { props.isAuth ? props.login
+        : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
     </header>
 )
 
