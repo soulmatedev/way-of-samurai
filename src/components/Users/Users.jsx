002750b7bdc61759a623ec11b4acbd123c9693e6
soulmatedev <khokhlov.dev@gmail.com>
2024-02-13 20:12:07 +0500

    Добавляет follow-unfollow для пользователей

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user.png";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
 	let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -47,10 +48,32 @@ let Users = (props) => {
 						{
 							user.followed
 								? <button onClick={() => {
-									props.unfollow(user.id)
+									axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {
+										withCredentials: true,
+										headers: {
+											"API-KEY": "189c3e8c-a494-4c55-922a-acbd41e43453"
+										}
+									})
+										.then(response => {
+											if (response.data.resultCode === 0) {
+												props.unfollow(user.id)
+											}
+										});
 								}}>unfollow</button>
+
 								: <button onClick={() => {
-									props.follow(user.id)
+									axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {},
+										{
+											withCredentials: true,
+											headers: {
+												"API-KEY": "189c3e8c-a494-4c55-922a-acbd41e43453"
+											}
+										})
+										.then(response => {
+											if (response.data.resultCode === 0) {
+												props.follow(user.id)
+											}
+										});
 								}}>follow</button>
 						}
 					</div>
