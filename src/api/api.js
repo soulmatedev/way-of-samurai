30e761ebb45d91d02223d03534bf08fa5f3b42be
soulmatedev <khokhlov.dev@gmail.com>
2024-02-16 20:27:02 +0500

    Добавляет слой DAL

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const instance = axios.create({
+	withCredentials: true,
+	baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+	headers: {
+		"API-KEY": "189c3e8c-a494-4c55-922a-acbd41e43453"
+	},
+})
+
+export const usersAPI = {
+	getUsers(currentPage = 1, pageSize = 10) {
+		return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+			.then(response => {
+				return response.data;
+			})
+	}
+}
+
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+	return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+		.then(response => {
+			return response.data;
+		})
+}
